cabc05fb2ab490983d930edeed2a00d5f780465f
TanviKakadiya <tanvikakadiya231@gmail.com>
2025-09-20 17:14:08 +0530

    Today's Problem
    Today's Problem

---
+++ Correctness and the Loop Invariant.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+
+void insertionSort(int N, int arr[]) {
+    int i, j, value;
+
+    for (i = 1; i < N; i++) {
+        value = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && value < arr[j]) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = value;
+    }
+
+    // Print only once when fully sorted
+    for (j = 0; j < N; j++) {
+        printf("%d", arr[j]);
+        if (j < N - 1) printf(" ");
+    }
+    printf("\n");
+}
+
+int main(void) {
+    int N;
+    scanf("%d", &N);
+
+    int arr[N], i;
+    for (i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    insertionSort(N, arr);
+
+    return 0;
+}
