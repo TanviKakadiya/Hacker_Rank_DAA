e251302cd1ef65e9c0b9b385e6cce5d3943bbc8b
TanviKakadiya <tanvikakadiya231@gmail.com>
2025-11-03 22:16:44 +0530

    Today's Problem
    Today's Problem

---
+++ Lily's Homework.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'lilysHomework' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+int lilysHomework(vector<int> arr) {
+    auto countSwaps = [&](const vector<int>& a, bool ascending) {
+        int n = a.size();
+        vector<pair<int,int>> v(n);
+        for (int i = 0; i < n; ++i) v[i] = {a[i], i};
+
+        if (ascending) {
+            sort(v.begin(), v.end(), [](const pair<int,int>& p, const pair<int,int>& q){
+                return p.first < q.first;
+            });
+        } else {
+            sort(v.begin(), v.end(), [](const pair<int,int>& p, const pair<int,int>& q){
+                return p.first > q.first;
+            });
+        }
+
+        vector<bool> visited(n, false);
+        int swaps = 0;
+        for (int i = 0; i < n; ++i) {
+            if (visited[i] || v[i].second == i) continue;
+            int cycle_size = 0;
+            int j = i;
+            while (!visited[j]) {
+                visited[j] = true;
+                j = v[j].second;
+                ++cycle_size;
+            }
+            if (cycle_size > 0) swaps += (cycle_size - 1);
+        }
+        return swaps;
+    };
+
+    return min(countSwaps(arr, true), countSwaps(arr, false));
+}
+
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int result = lilysHomework(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
